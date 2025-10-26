59ad4321f31959bd8ed8265cde09c5b4e9b18962
Parzivak <bherrera@unicauca.edu.co>
2025-10-25 22:17:11 -0500

    se quito webconfig y se creo corsconfig para manejar cors en el api gateway

---
+++ apigateway/src/main/java/com/microservicio/apigateway/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.microservicio.apigateway.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        
+        // --- URLS PERMITIDAS ---
+        // Aquí debes poner tus URLs de frontend
+        corsConfig.setAllowedOrigins(Arrays.asList(
+            "http://localhost:3000",
+            "https://frontend-santabarbara-m.vercel.app" // Tu URL de Vercel
+        ));
+        
+        corsConfig.setMaxAge(3600L); // 1 hora
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // Aplica a todas las rutas
+
+        return new CorsWebFilter(source);
+    }
+}
